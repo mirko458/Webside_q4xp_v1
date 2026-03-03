4f04321d806c477934f91043d584fa22d2f5d1bd
Mirko Quintern <mirko.quintern@gmail.com>
2026-03-03 15:45:24 +0100

    Update vite config with base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname
-    }
-  }
-})
+import { defineConfig } from "vite"  
+import react from "@vitejs/plugin-react"  
+export default defineConfig({  
+  base: "/Webside_q4xp_v1/",  
+  plugins: [react()],  
+  resolve: {  
+    alias: {  
+      "@": new URL("./src", import.meta.url).pathname  
+    }  
+  }  
+}) 
